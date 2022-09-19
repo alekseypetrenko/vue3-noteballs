5012afc3c6fe4140fb9e0d08a5efca975f232976
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-09-19 14:08:26 +0300

    add bulma, nav bar, notes design

--- src/main.js
+++ src/main.js
@@ -1,26 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-
-import ViewNotes from "@/views/ViewNotes.vue";
-import ViewStats from "@/views/ViewStats.vue";
-
-import "./assets/main.css";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "notes",
-      component: ViewNotes,
-    },
-    {
-      path: "/stats",
-      name: "stats",
-      component: ViewStats,
-    },
-  ],
-});
+import router from "@/router";
 
 createApp(App).use(router).mount("#app");
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,3 +1,33 @@
 <template>
-  <div class="notes">notes</div>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          aliquam necessitatibus officiis, dolorum dolorem officia odit, ipsum
+          obcaecati quisquam fugiat dignissimos rerum fuga perferendis sed ex,
+          velit dolores eius molestiae!
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
 </template>
